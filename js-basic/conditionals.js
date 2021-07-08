46125b0628eacc65a7f325daef1c8ffbaec99b90
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-07-08 15:03:36 -0300

    conditionals

---
+++ js-basic/conditionals.js
@@ -0,0 +1,57 @@
+// const ianAge = 23;
+// const jonatasAge = 22;
+
+// if (ianAge > jonatasAge) {
+//   console.log("Ian é mais velho");
+// } else if (ianAge == jonatasAge) {
+//   console.log("Os dois tem a mesma idade");
+// } else {
+//   console.log("Jonatas é mais velho");
+// }
+
+//Condicional com Switch Case
+// function getCadarpio(dayOfTheWeek) {
+//   let cardapio;
+//   switch (dayOfTheWeek) {
+//     case "Saturday":
+//       cardapio = "Japanese Food";
+//       break;
+//     case "Sunday":
+//       cardapio = "Italian Food";
+//       break;
+//     default:
+//       return "Não atendemos neste dia";
+//   }
+
+//   return cardapio;
+// }
+
+// //getCadarpio("Saturday");
+// console.log(getCadarpio("Saturday"));
+// console.log(getCadarpio("Sunday"));
+// console.log(getCadarpio("Friday"));
+// console.log(getCadarpio("Tuesday"));
+// console.log(getCadarpio("Monday"));
+//
+
+const value1 = 13;
+const value2 = "13";
+
+// if (value1 === value2) {
+//   console.log("Os valores e tipos são iguais");
+// }
+// if (value1 == value2) {
+//   console.log("Apenas os valores são iguais");
+// }
+
+// console.log(typeof value1);
+// console.log(typeof value2);
+
+// console.log(value1 === value2);
+// console.log(value1 == value2);
+
+const operationResult1 = value1 === value2; // Os valores e tipos são iguais
+const operationResult2 = value1 == value2; // Apenas os valores são iguais
+
+console.log(operationResult1);
+console.log(operationResult2);
