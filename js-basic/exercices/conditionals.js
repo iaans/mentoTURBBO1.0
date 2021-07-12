3129ee7ccbec288a346d2b0227fb18b8dd104f69
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-07-12 10:13:58 -0300

    Exercicios realizados. Duvidas no Switch Case

--- js-basic/exercices/conditionals.js
+++ js-basic/exercices/conditionals.js
@@ -1,7 +1,34 @@
 //SwtichCase
+/* function finalEuroLeague(getChampion) {
+  let team;
+  switch (getChampion) {
+    case "Italia":
+      team = "Bicampeã";
+      break;
+    case "Inglaterra":
+      team = "Vice campeã";
+      break;
+    default:
+      return "Foi eliminado";
+  }
+  return team;
+}
+getChampion("Italia");
+console.log(getChampion("Italia"));
+*/
 
 //If&Else
+const value1 = 30;
+const value2 = "30";
+
+if (value1 === value2) {
+  console.log("Os valores e tipos são iguais");
+}
+if (value1 == value2) {
+  console.log("Apenas os valores são iguais");
+}
 
 //Equals x2
-
+console.log(value1 == value2);
 //Equals x3
+console.log(value1 === value2);
