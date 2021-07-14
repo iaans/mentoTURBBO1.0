74abb764e7c7c489b24bc1ca0a9cace7925445dd
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-07-14 11:31:13 -0300

    Exercicios resolvidos

--- js-basic/activity/act1.js
+++ js-basic/activity/act1.js
@@ -10,5 +10,9 @@ const getFruits = fruits[2];
 console.log("Last element of the array:", getFruits);
 
 // B) Get the first element of the array
-const getFruits = fruits[0];
-console.log("First element of the array:", getFruits);
+const getFruits1 = fruits[0];
+console.log("First element of the array:", getFruits1);
+
+// C) Get the last element of the array ever added an item
+const getFruits2 = fruits[fruits.length - 1];
+console.log("Last element of the array:", getFruits2);
--- js-basic/activity/act2.js
+++ js-basic/activity/act2.js
@@ -4,15 +4,29 @@ const numbers = [3, 7, 2, 8, 1];
 // A) Sort them in ascending order [1, 2, 3, 7, 8]
 // B) Reverse the numbers [1, 8, 2, 7, 3]
 // C) Push new these numbers to the array => 10, 99, 55, 77
+// D) Get the array updated
+// E Get odd numbers and even numbers.
 
 // SHOW ALL THE RESULTS ON THE SCREEN (CONSOLE.LOG)
-
+console.log("Array of numbers:");
+console.log(numbers);
 //A
-console.log("Numbers ascending order: ", numbers.sort());
+console.log("Numbers ascending order:\n", numbers.sort());
 
 //B
-console.log("Numbers reverse: ", numbers.reverse());
+console.log("Numbers reverse:\n", numbers.reverse());
 
 //C
 numbers.push(10, 99, 55, 77);
-console.log(numbers);
+console.log("Add numbers:\n" + numbers);
+
+//D
+const getNumbers = numbers[numbers.length - 1];
+console.log("Last element of the array:", getNumbers);
+
+//E
+let odds = numbers.filter((num) => num % 2);
+console.log("Odd numbers: ", odds);
+
+// let even = numbers.filter((n) => n * 2);
+// console.log("Even numbers: ", even);
