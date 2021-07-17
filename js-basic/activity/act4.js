557b1ef5c2e986bf124f089bc892cd9e0e80d195
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-07-17 14:13:05 -0300

    Update repo 17072021

--- js-basic/activity/act4.js
+++ js-basic/activity/act4.js
@@ -1,35 +1,11 @@
-// 2) Given this array of numbers:
-const numbers = [3, 7, 2, 8, 1];
+// EXERCICES LOOP
 
-// A) Sort them in ascending order [1, 2, 3, 7, 8]
-// B) Reverse the numbers [1, 8, 2, 7, 3]
-// C) Push new these numbers to the array => 10, 99, 55, 77
-// D) Get the last element of the array
-// E) Get the array updated
-// F) Get odd numbers and even numbers.
+// 1) Implement a for loop that increases 2 numbers in each loop and print the numbers on the screen
 
-// SHOW ALL THE RESULTS ON THE SCREEN (CONSOLE.LOG)
-console.log(`Array of numbers: ${numbers}`);
+// 2) Get an array of numbers, loop through them in a foreach loop, and in the end show the sum of all the numbers
 
-//A
-console.log("Numbers ascending order: ↓ \n", numbers.sort());
-//B
-console.log("Numbers reverse: ↓ \n", numbers.reverse());
-//C
-numbers.push(10, 99, 55, 77);
-//console.log(`Add numbers: ↓ \n `, numbers);
-console.log("Add numbers: ↓ \n", numbers);
+// const numbers = [10, 20, 30, 22, 42, 99]
 
-//D
-const lastElement = numbers[numbers.length - 1];
-console.log("Last element of the array is: ↓ \n", lastElement);
+// 3) Implement a do while loop to print the values of the const counter = 0; on the screen, until it reaches 10, remember to increment the counter on each loop
 
-//E
-console.log("Array update: ↓ \n", numbers); //?
-
-//F
-let odds = numbers.filter((num) => num % 2);
-console.log("Odds numbers: ↓ \n", odds);
-
-let even = numbers.filter((num) => !(num % 2));
-console.log("Even numbers: ↓ \n", even);
+// 4) Do the same as exercice 3 but with a while loop
